6526ee10d06d4ef08430563ebf041799103de6a0
thgank <sultanhuman02@gmail.com>
2024-03-06 22:08:11 +0500

    feat: optimize templates parsing

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -26,40 +26,46 @@ type templateData struct {
 	IframeSrc       string
 }
 
-func humanDate(t time.Time) string {
-	return t.Format("02 Jan 2006 at 15:04")
-}
-
 var functions = template.FuncMap{
 	"humanDate": humanDate,
 }
 
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
 func newTemplateCache(dir string) (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
-	pages, err := filepath.Glob(filepath.Join(dir, "*.page.html"))
+
+	layoutFiles, err := filepath.Glob(filepath.Join(dir, "*.layout.tmpl"))
 	if err != nil {
 		return nil, err
 	}
 
-	for _, page := range pages {
+	partialFiles, err := filepath.Glob(filepath.Join(dir, "*.partial.html"))
+	if err != nil {
+		return nil, err
+	}
+
+	// combine layout and partial files which will be included in every page template
+	templateFiles := append(layoutFiles, partialFiles...)
+
+	pageFiles, err := filepath.Glob(filepath.Join(dir, "*.page.html"))
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pageFiles {
 		name := filepath.Base(page)
 
-		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
-		if err != nil {
-			return nil, err
-		}
-
-		ts, err = ts.ParseGlob(filepath.Join(dir, "*.layout.tmpl"))
-		if err != nil {
-			return nil, err
-		}
-
-		ts, err = ts.ParseGlob(filepath.Join(dir, "*.partial.html"))
+		// parse the page template along with the layout and partial templates
+		ts, err := template.New(name).Funcs(functions).ParseFiles(append(templateFiles, page)...)
 		if err != nil {
 			return nil, err
 		}
 
 		cache[name] = ts
 	}
+
 	return cache, nil
 }
